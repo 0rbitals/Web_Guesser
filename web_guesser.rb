f39c71efbc3d305286961fd61bd85dc772222182
Elektron@Amaterasu <electroniknight@gmail.com>
2016-08-07 18:36:51 +0100

    Completed Guessing Logic

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,24 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-random_num = rand(101)
+RANDOM_NUM = rand(100)
+
+def check_guess(guess)
+  if params['guess'].to_i > RANDOM_NUM + 5
+    msg = "Way too high!"
+  elsif params['guess'].to_i > RANDOM_NUM
+    msg = "Too high!"
+  elsif params['guess'].to_i < RANDOM_NUM - 5
+    msg = "Way too low!"
+  elsif params['guess'].to_i < RANDOM_NUM
+    msg = "Too low!"
+  else
+    msg = "That's correct!"
+  end
+end
 
 get '/' do
-  erb :index, :locals => {:number => random_num}
+  guess = params["guess"]
+  msg = check_guess(guess)
+  erb :index, :locals => {:number => RANDOM_NUM, :message => msg}
 end
